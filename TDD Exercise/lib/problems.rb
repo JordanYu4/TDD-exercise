92b3722e316c9299faa14d2b5fe7538dc334998b
JordanYu4 <jordanyu44@gmail.com>
2018-06-27 14:18:12 -0700

    Finish Problems.rb

--- TDD Exercise/lib/problems.rb
+++ TDD Exercise/lib/problems.rb
@@ -8,7 +8,39 @@ class Array
   
   def two_sum
     pairs = []
-    
+    self.length.times do |first|
+      self.length.times do |second|
+        next if second <= first
+        if self[first] + self[second] == 0
+          pairs << [first, second]
+        end
+      end
+    end
+    pairs
   end 
   
+  def my_transpose
+    transposed = Array.new(self.length) {[]}
+    self.each do |row|
+      row.each_with_index do |el, col|
+        transposed[col] << el 
+      end 
+    end 
+    transposed
+  end
+  
+  def stock_picker
+    pair = [0,0]
+    self.length.times do |buy|
+      self.length.times do |sell|
+        next if sell <= buy
+        if self[sell] - self[buy] > self[pair.last] - self[pair.first]
+          pair = [buy, sell]
+        end
+      end
+    end
+    return [] if pair.uniq.length == 1
+    pair
+  end
+    
 end
--- TDD Exercise/spec/problems_spec.rb
+++ TDD Exercise/spec/problems_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Problems" do
     let(:pairs) {[[0, 4], [2, 3]]}
     
     it "returns positions of pairs of numbers that add to zero" do
-      expect([1, 2, 3, -2].two_sum).to eq([1,3])
+      expect([1, 2, 3, -2].two_sum).to eq([[1,3]])
     end
 
     it "finds multiple pairs in the right order" do
@@ -37,4 +37,35 @@ RSpec.describe "Problems" do
     end
     
   end
+  
+  describe "#my_transpose" do 
+    let(:arr) {[[0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]]}
+    let(:transposed) {[[0, 3, 6],
+                       [1, 4, 7],
+                       [2, 5, 8]]}
+    
+    it "transposes a matrix" do 
+      expect(arr.my_transpose).to eq(transposed)
+    end
+    
+    it "returns empty array if input is empty" do 
+      expect([].my_transpose).to eq([])
+    end 
+    
+    it "returns the same array if transposed twice" do 
+      expect(arr.my_transpose.my_transpose).to eq(arr)
+    end 
+  end
+  
+  describe "#stock_picker" do 
+    it "returns the best days to buy & sell" do 
+      expect([3,1,2,7,6].stock_picker).to eq([1,3])
+    end 
+    
+    it "returns empty array if stock price never increases" do 
+      expect([4,4,4,4,4].stock_picker).to eq([])
+    end
+  end 
 end
